b25445996a408dc8daccdebc7972dcbdeb3370a3
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-19 15:20:11 +0300

    Add dashboard page

---
+++ src/app/(dashboard)/page.tsx
@@ -0,0 +1,10 @@
+const DashboardPage = () => {
+  return (
+    <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
+      <h1 className='mb-2 text-3xl font-bold tracking-tight'>Dashboard</h1>
+      <p>You shouldn&apos;t be here unless you&apos;re authenticated.</p>
+    </div>
+  );
+};
+
+export default DashboardPage;
--- src/app/page.tsx
+++
@@ -1,7 +0,0 @@
-const HomePage = () => {
-  return (
-    <div className='grid min-h-dvh w-full place-items-center'>Hello world.</div>
-  );
-};
-
-export default HomePage;
