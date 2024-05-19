7c7254e7f41e6d10d8ce407726220225b358ee4f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-19 15:20:27 +0300

    Add auth pages

---
+++ src/app/(auth)/signin/[[...signin]]/page.tsx
@@ -0,0 +1,9 @@
+const SignInPage = () => {
+  return (
+    <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
+      Sign in
+    </div>
+  );
+};
+
+export default SignInPage;
---
+++ src/app/(auth)/signup/page.tsx
@@ -0,0 +1,9 @@
+const SignUpPage = () => {
+  return (
+    <div className='flex min-h-dvh w-full flex-col items-center justify-center'>
+      Sign up
+    </div>
+  );
+};
+
+export default SignUpPage;
