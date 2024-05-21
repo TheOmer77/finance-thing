87afd29a8c02fbce6dc0143cbc719c46c339553d
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-21 17:12:35 +0300

    Add auth protection to api routes

--- src/app/api/[[...route]]/route.ts
+++ src/app/api/[[...route]]/route.ts
@@ -1,10 +1,22 @@
 import { Hono } from 'hono';
 import { handle } from 'hono/vercel';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 
 const app = new Hono().basePath('/api');
 
+app.use(
+  '*',
+  clerkMiddleware({
+    secretKey: process.env.CLERK_SECRET_KEY,
+    publishableKey: process.env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY,
+  })
+);
 app.get('/', ctx => {
-  return ctx.json({ message: 'Hello Next.js!' });
+  const auth = getAuth(ctx);
+  if (!auth?.userId)
+    return ctx.json({ success: false, message: "You're not signed in" });
+
+  return ctx.json({ message: 'Hello Next.js!', userId: auth.userId });
 });
 
 export const GET = handle(app);
