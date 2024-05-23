a9b121af082eb75275926ae4eb64ee9e1989739b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-23 18:37:47 +0300

    Add accounts POST api route

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,15 +1,15 @@
 import { Hono } from 'hono';
 import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
 import { HTTPException } from 'hono/http-exception';
+import { zValidator } from '@hono/zod-validator';
 import { eq } from 'drizzle-orm';
 
 import { db } from '@/db';
-import { accounts } from '@/db/schema';
+import { accounts, insertAccountSchema } from '@/db/schema';
 
-export const accountsRouter = new Hono().get(
-  '/',
-  clerkMiddleware(),
-  async ctx => {
+export const accountsRouter = new Hono()
+
+  .get('/', clerkMiddleware(), async ctx => {
     const auth = getAuth(ctx);
     if (!auth?.userId)
       throw new HTTPException(401, { message: 'Unauthorized' });
@@ -19,5 +19,23 @@ export const accountsRouter = new Hono().get(
       .from(accounts)
       .where(eq(accounts.userId, auth.userId));
     return ctx.json({ success: true, data });
-  }
-);
+  })
+
+  .post(
+    '/',
+    clerkMiddleware(),
+    zValidator('json', insertAccountSchema.pick({ name: true })),
+    async ctx => {
+      const auth = getAuth(ctx);
+      if (!auth?.userId)
+        throw new HTTPException(401, { message: 'Unauthorized' });
+
+      const { name } = ctx.req.valid('json');
+
+      const [data] = await db
+        .insert(accounts)
+        .values({ name, userId: auth.userId })
+        .returning();
+      return ctx.json({ success: true, data });
+    }
+  );
