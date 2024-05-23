3bdcc16d508a15565f47e387588c54a828be60bc
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-23 16:29:24 +0300

    Check auth in accounts api route

--- src/app/api/[[...route]]/accounts.ts
+++ src/app/api/[[...route]]/accounts.ts
@@ -1,11 +1,23 @@
 import { Hono } from 'hono';
+import { clerkMiddleware, getAuth } from '@hono/clerk-auth';
+import { HTTPException } from 'hono/http-exception';
+import { eq } from 'drizzle-orm';
 
 import { db } from '@/db';
 import { accounts } from '@/db/schema';
 
-export const accountsRouter = new Hono().get('/', async ctx => {
-  const data = await db
-    .select({ id: accounts.id, name: accounts.name })
-    .from(accounts);
-  return ctx.json({ success: true, data });
-});
+export const accountsRouter = new Hono().get(
+  '/',
+  clerkMiddleware(),
+  async ctx => {
+    const auth = getAuth(ctx);
+    if (!auth?.userId)
+      throw new HTTPException(401, { message: 'Unauthorized' });
+
+    const data = await db
+      .select({ id: accounts.id, name: accounts.name })
+      .from(accounts)
+      .where(eq(accounts.userId, auth.userId));
+    return ctx.json({ success: true, data });
+  }
+);
