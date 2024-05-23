b3096944749ffa7bdda2409b2ce5606a817d8167
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-23 16:31:59 +0300

    Remove `h1` from dashboard layout

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,10 +5,7 @@ import { Header } from '@/components/layout/Header';
 const DashboardLayout = ({ children }: PropsWithChildren) => (
   <>
     <Header />
-    <main className='mx-auto w-full max-w-screen-2xl px-4'>
-      <h1 className='mb-2 text-3xl font-bold tracking-tight'>Dashboard</h1>
-      {children}
-    </main>
+    <main className='mx-auto w-full max-w-screen-2xl px-4'>{children}</main>
   </>
 );
 
