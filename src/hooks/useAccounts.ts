cf8101780341afe4a9dd2e630840f9c7fa358860
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-24 15:05:28 +0300

    Add create account mutation in useAccounts

--- src/hooks/useAccounts.ts
+++ src/hooks/useAccounts.ts
@@ -1,8 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import type { InferResponseType, InferRequestType } from 'hono';
 
 import { client } from '@/lib/hono';
 
 export const useAccounts = () => {
+  const queryClient = useQueryClient();
+
   const getAccountsQuery = useQuery({
     queryKey: ['accounts'],
     queryFn: async () => {
@@ -14,8 +18,26 @@ export const useAccounts = () => {
     },
   });
 
+  const createAccountMutation = useMutation<
+    InferResponseType<typeof client.api.accounts.$post>,
+    Error,
+    InferRequestType<typeof client.api.accounts.$post>['json']
+  >({
+    mutationFn: async json => {
+      const res = await client.api.accounts.$post({ json });
+      return await res.json();
+    },
+    onSuccess: () => {
+      toast.success('Account created.');
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
+    },
+    onError: () => toast.error('Failed to create account.'),
+  });
+
   return {
     accounts: getAccountsQuery.data,
     accountsLoading: getAccountsQuery.isLoading,
+    createAccount: createAccountMutation.mutate,
+    createAccountPending: createAccountMutation.isPending,
   };
 };
