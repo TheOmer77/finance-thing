2ed04d34e17de10c7bf959f39856c62267e3620b
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-24 21:04:00 +0300

    Move content after header up

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -6,8 +6,8 @@ import { HeaderWelcomeMsg } from './HeaderWelcomeMsg';
 export const Header = () => (
   <header
     className='h-64 w-full bg-background bg-gradient-to-b from-primary
-to-primary/80 text-primary-foreground lg:h-80 [&>*]:mx-auto [&>*]:w-full
-[&>*]:max-w-screen-2xl [&>*]:px-4'
+to-primary/80 text-primary-foreground lg:h-80 [&+*]:-mt-24 [&>*]:mx-auto
+[&>*]:w-full [&>*]:max-w-screen-2xl [&>*]:px-4'
   >
     <div
       className='flex h-16 items-center justify-start gap-2 lg:flex-row
