fd12480ca99a51f86bf0bcd419b05382dd22812e
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-25 21:58:08 +0300

    Add update account mutation in useAccount

--- src/hooks/useAccount.ts
+++ src/hooks/useAccount.ts
@@ -1,8 +1,12 @@
-import { useQuery } from '@tanstack/react-query';
+import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
+import { toast } from 'sonner';
+import type { InferRequestType, InferResponseType } from 'hono';
 
 import { client } from '@/lib/hono';
 
 export const useAccount = (id?: string) => {
+  const queryClient = useQueryClient();
+
   const getQuery = useQuery({
     enabled: !!id,
     queryKey: ['account', { id }],
@@ -15,5 +19,31 @@ export const useAccount = (id?: string) => {
     },
   });
 
-  return { account: getQuery.data, accountFetching: getQuery.isFetching };
+  const updateMutation = useMutation<
+    InferResponseType<(typeof client.api.accounts)[':id']['$patch']>,
+    Error,
+    InferRequestType<(typeof client.api.accounts)[':id']['$patch']>['json']
+  >({
+    mutationFn: async json => {
+      const res = await client.api.accounts[':id'].$patch({
+        param: { id },
+        json,
+      });
+      return await res.json();
+    },
+    onSuccess: () => {
+      toast.success('Account updated.');
+      queryClient.invalidateQueries({ queryKey: ['accounts'] });
+      queryClient.invalidateQueries({ queryKey: ['account', { id }] });
+      // TODO: Invalidate summary and transactions
+    },
+    onError: () => toast.error('Failed to update account.'),
+  });
+
+  return {
+    account: getQuery.data,
+    accountFetching: getQuery.isFetching,
+    updateAccount: updateMutation.mutate,
+    updateAccountPending: updateMutation.isPending,
+  };
 };
