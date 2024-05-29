90345a2cd4a7fdf2b7e49ae0b5f347ca1b140dec
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-05-29 20:37:44 +0300

    Add categories DB schema

---
+++ src/db/schema/categoties.ts
@@ -0,0 +1,21 @@
+import { pgTable, text, timestamp } from 'drizzle-orm/pg-core';
+import { createInsertSchema } from 'drizzle-zod';
+import { createId } from '@paralleldrive/cuid2';
+
+export const categories = pgTable('categories', {
+  id: text('id').primaryKey().$default(createId),
+  userId: text('user_id').notNull(),
+  name: text('name').notNull(),
+  createdAt: timestamp('created_at').notNull().defaultNow(),
+  updatedAt: timestamp('updated_at')
+    .notNull()
+    .defaultNow()
+    .$onUpdate(() => new Date()),
+});
+
+export const insertCategorySchema = createInsertSchema(categories, {
+  name: ({ name }) => name.min(1, { message: 'Name is required.' }),
+}).omit({
+  createdAt: true,
+  updatedAt: true,
+});
