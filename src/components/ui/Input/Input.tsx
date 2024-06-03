f0d47f84bb117f3d87ea64506526b3a549ca6aab
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-03 17:09:15 +0300

    Add `asChild` prop to Input

--- src/components/ui/Input/Input.tsx
+++ src/components/ui/Input/Input.tsx
@@ -3,27 +3,32 @@ import {
   type ComponentPropsWithoutRef,
   type ElementRef,
 } from 'react';
+import { Slot } from '@radix-ui/react-slot';
 
 import { cn } from '@/lib/utils';
 
-export const Input = forwardRef<
-  ElementRef<'input'>,
-  ComponentPropsWithoutRef<'input'>
->(({ className, type, ...props }, ref) => {
-  return (
-    <input
-      {...props}
-      ref={ref}
-      type={type}
-      className={cn(
-        `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2
+export type InputProps = ComponentPropsWithoutRef<'input'> & {
+  asChild?: boolean;
+};
+
+export const Input = forwardRef<ElementRef<'input'>, InputProps>(
+  ({ className, type, asChild, ...props }, ref) => {
+    const Comp = asChild ? Slot : 'input';
+    return (
+      <Comp
+        {...props}
+        ref={ref}
+        type={type}
+        className={cn(
+          `flex h-10 w-full rounded-md border border-input bg-background px-3 py-2
 text-sm ring-offset-background file:border-0 file:bg-transparent file:text-sm
 file:font-medium placeholder:text-muted-foreground focus-visible:outline-none
 focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2
 disabled:cursor-not-allowed disabled:opacity-50`,
-        className
-      )}
-    />
-  );
-});
+          className
+        )}
+      />
+    );
+  }
+);
 Input.displayName = 'Input';
