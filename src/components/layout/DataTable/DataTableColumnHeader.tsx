cb3e04de912d59b9986c52af1b761db627f01658
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-05 21:31:38 +0300

    Tweak classname in DataTableColumnHeader

--- src/components/layout/DataTable/DataTableColumnHeader.tsx
+++ src/components/layout/DataTable/DataTableColumnHeader.tsx
@@ -22,7 +22,7 @@ export function DataTableColumnHeader<TData, TValue>({
       <Button
         variant='flat'
         size='sm'
-        className='-ml-3 h-8 data-[state=open]:bg-accent'
+        className='-ms-3 h-8 data-[state=open]:bg-accent'
         onClick={() => column.toggleSorting(column.getIsSorted() === 'asc')}
       >
         <span>{title}</span>
