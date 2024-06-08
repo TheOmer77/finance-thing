6056cf99c8b7f32db7ce10c7a93543550d1eb540
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-08 14:58:51 +0300

    Parse selected CSV columns when clicking continue

--- src/app/(dashboard)/transactions/_components/header/index.tsx
+++ src/app/(dashboard)/transactions/_components/header/index.tsx
@@ -3,27 +3,38 @@
 import { useSearchParams } from 'next/navigation';
 import { CheckIcon, PlusIcon, XIcon } from 'lucide-react';
 import type { ParseResult } from 'papaparse';
+import { format, parse } from 'date-fns';
+import { toast } from 'sonner';
 
 import { Button } from '@/components/ui/Button';
 import { CardDescription, CardHeader, CardTitle } from '@/components/ui/Card';
 import { Skeleton } from '@/components/ui/Skeleton';
 import { useModal } from '@/hooks/useModal';
 import { useTransactions, useTransactionsImport } from '@/hooks/transactions';
+import { amountToMilliunits } from '@/lib/amount';
 
 import { UploadButton } from './upload-button';
-import { REQUIRED_FIELDS } from '../table/constants';
+import {
+  FIELDS,
+  INPUT_DATE_FORMAT,
+  OUTPUT_DATE_FORMAT,
+  REQUIRED_FIELDS,
+} from '../table/constants';
 
 export const TransactionsHeader = () => {
   const { transactionsLoading } = useTransactions();
   const { importResult, setImportResult, selectedColumns, resetImport } =
-    useTransactionsImport();
+      useTransactionsImport(),
+    [headers, ...body] = importResult.data;
 
   const searchParams = useSearchParams(),
     isImportMode = !!searchParams.get('import') && importResult.data.length > 0;
   const { openModal } = useModal();
 
   const progress = Object.values(selectedColumns).filter(
-      value => value && REQUIRED_FIELDS.includes(value)
+      value =>
+        value &&
+        REQUIRED_FIELDS.includes(value as (typeof REQUIRED_FIELDS)[number])
     ).length,
     progressLeft = REQUIRED_FIELDS.length - progress,
     canContinueImport = progress >= REQUIRED_FIELDS.length;
@@ -31,6 +42,67 @@ export const TransactionsHeader = () => {
   const handleUpload = (result: ParseResult<string[]>) => {
     window.history.pushState(null, '', `?import=true`);
     setImportResult(result);
+  };
+
+  const handleImportContinue = () => {
+    const mappedData = {
+      headers: [...Array(headers.length).keys()].map(
+        index => selectedColumns[`column-${index}`] || null
+      ),
+      body: body
+        .map(row => {
+          const transformedRow = row.map((cell, index) =>
+            selectedColumns[`column-${index}`] ? cell : null
+          );
+          return transformedRow.every(item => item === null)
+            ? []
+            : transformedRow;
+        })
+        .filter(row => row.length > 0),
+    };
+
+    try {
+      const formattedData = mappedData.body.map(row =>
+        row.reduce((obj, value, index) => {
+          const header = mappedData.headers[index] as
+            | (typeof FIELDS)[number]
+            | null;
+          if (header === null || value === null) return obj;
+
+          switch (header) {
+            case 'amount': {
+              const formattedValue = amountToMilliunits(Number(value));
+              if (isNaN(formattedValue))
+                throw new Error(
+                  'The column selected for amount contains an invalid number.'
+                );
+              return { ...obj, [header]: formattedValue };
+            }
+            case 'date': {
+              try {
+                const formattedValue = format(
+                  parse(value, INPUT_DATE_FORMAT, new Date()),
+                  OUTPUT_DATE_FORMAT
+                );
+                return { ...obj, [header]: formattedValue };
+              } catch (error) {
+                throw new Error(
+                  `The column selected for date contains an invalid date, or
+it's in an incorrect format.`
+                );
+              }
+            }
+            default:
+              return { ...obj, [header]: value };
+          }
+        }, {})
+      );
+
+      console.log(formattedData);
+    } catch (error) {
+      if (error instanceof Error) return toast.error(error.message);
+      toast.error('Something went wrong while parsing your imported data.');
+    }
   };
 
   const handleImportCancel = () => {
@@ -72,6 +144,7 @@ export const TransactionsHeader = () => {
               <Button
                 variant='primary'
                 className='w-10 px-0 sm:w-auto sm:px-4'
+                onClick={handleImportContinue}
                 disabled={!canContinueImport}
               >
                 <CheckIcon className='size-4 sm:me-2' />
--- src/app/(dashboard)/transactions/_components/table/constants.ts
+++ src/app/(dashboard)/transactions/_components/table/constants.ts
@@ -1,4 +1,5 @@
 export const INPUT_DATE_FORMAT = 'yyyy-MM-dd HH:mm:ss',
   OUTPUT_DATE_FORMAT = 'yyyy-MM-dd';
-export const REQUIRED_FIELDS = ['amount', 'date', 'payee'],
-  FIELDS = [...REQUIRED_FIELDS, 'notes'];
+export const REQUIRED_FIELDS = ['amount', 'date', 'payee'] as const,
+  OPTIONAL_FIELDS = ['notes'] as const,
+  FIELDS = [...REQUIRED_FIELDS, ...OPTIONAL_FIELDS] as const;
