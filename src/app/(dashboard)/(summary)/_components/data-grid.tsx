0b68946e3ccb23149a6ed272c80681a4ac8d0a33
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-17 19:37:30 +0300

    Move wrapping div into DataGrid

--- src/app/(dashboard)/(summary)/_components/data-grid.tsx
+++ src/app/(dashboard)/(summary)/_components/data-grid.tsx
@@ -1,9 +1,9 @@
 import { DataCard } from './data-card';
 
 export const DataGrid = () => (
-  <>
+  <div className='mb-8 grid grid-cols-1 gap-8 pb-2 lg:grid-cols-3'>
     <DataCard title='Remaining' type='remaining' />
     <DataCard title='Income' type='income' />
     <DataCard title='Expenses' type='expenses' />
-  </>
+  </div>
 );
--- src/app/(dashboard)/(summary)/page.tsx
+++ src/app/(dashboard)/(summary)/page.tsx
@@ -1,9 +1,9 @@
 import { DataGrid } from './_components/data-grid';
 
 const DashboardPage = () => (
-  <div className='mb-8 grid grid-cols-1 gap-8 pb-2 lg:grid-cols-3'>
+  <>
     <DataGrid />
-  </div>
+  </>
 );
 
 export default DashboardPage;
