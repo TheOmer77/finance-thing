59a85c76cc8eb7887766351ea693ac12c4b1d5af
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-17 19:42:06 +0300

    Add MaybeError type

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,5 +1,8 @@
 export const DATE_FORMAT = 'yyyy-MM-dd';
 
 export const PROMISE_REJECT_ERROR = { error: 'Something went wrong.' } as const;
+
+/** Either `T` or a generic error object.  */
+export type MaybeError<T> = T | typeof PROMISE_REJECT_ERROR;
 /** Either the awaited type of `T` or a generic error object.  */
-export type ResolvedOrError<T> = Awaited<T> | typeof PROMISE_REJECT_ERROR;
+export type ResolvedOrError<T> = MaybeError<Awaited<T>>;
