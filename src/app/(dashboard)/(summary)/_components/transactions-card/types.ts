dfe5bb6e36284a63a403f543aab4ae148140f757
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-21 14:32:56 +0300

    Use inferred API type for TransactionsDaysData type

--- src/app/(dashboard)/(summary)/_components/transactions-card/types.ts
+++ src/app/(dashboard)/(summary)/_components/transactions-card/types.ts
@@ -1,8 +1,11 @@
-export type TransactionsDaysData = {
-  date: string;
-  income: number;
-  expenses: number;
-}[];
+import type { InferResponseType } from 'hono';
+
+import type { client } from '@/lib/hono';
+import type { NotError } from '@/constants/api';
+
+export type TransactionsDaysData = NotError<
+  InferResponseType<typeof client.api.summary.$get>['data']['days']
+>;
 export type TransactionsChartProps = { data: TransactionsDaysData };
 
 export type TransactionsChartType = 'area' | 'bar' | 'line';
