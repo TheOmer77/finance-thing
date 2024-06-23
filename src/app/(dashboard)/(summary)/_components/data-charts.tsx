d3e43457261f0a353be2a9d4d2c88991b4f75e7c
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-23 19:36:58 +0300

    Add loading states to dashboard chart cards

--- src/app/(dashboard)/(summary)/_components/data-charts.tsx
+++ src/app/(dashboard)/(summary)/_components/data-charts.tsx
@@ -1,29 +1,9 @@
-'use client';
-
-import { Card, CardContent } from '@/components/ui/Card';
-import { Spinner } from '@/components/ui/Spinner';
-import { useSummary } from '@/hooks/summary';
-
 import { TransactionsChartCard } from './transactions-card';
 import { SpendingPieCard } from './spending-card';
 
-export const DataCharts = () => {
-  const { summaryLoading } = useSummary();
-
-  if (summaryLoading)
-    return (
-      <Card>
-        <CardContent className='pt-6 text-sm text-muted-foreground'>
-          {/* TEMPORARY! */}
-          <Spinner />
-        </CardContent>
-      </Card>
-    );
-
-  return (
-    <div className='grid grid-cols-1 gap-4 md:gap-8 lg:grid-cols-6'>
-      <TransactionsChartCard />
-      <SpendingPieCard />
-    </div>
-  );
-};
+export const DataCharts = () => (
+  <div className='grid grid-cols-1 gap-4 md:gap-8 lg:grid-cols-6'>
+    <TransactionsChartCard />
+    <SpendingPieCard />
+  </div>
+);
--- src/app/(dashboard)/(summary)/_components/transactions-card/index.tsx
+++ src/app/(dashboard)/(summary)/_components/transactions-card/index.tsx
@@ -1,6 +1,10 @@
+'use client';
+
 import { useState } from 'react';
 
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/Card';
+import { Skeleton } from '@/components/ui/Skeleton';
+import { Spinner } from '@/components/ui/Spinner';
 import { useSummary } from '@/hooks/summary';
 import { isErrorObj } from '@/lib/isErrorObj';
 import type { MaybeError } from '@/types/api';
@@ -44,10 +48,23 @@ const ChartCardContent = ({ data, type }: ChartCardContentProps) => {
   }
 };
 
+const TransactionsChartCardLoading = () => (
+  <Card className='col-span-1 lg:col-span-3 xl:col-span-4'>
+    <CardHeader className='flex flex-row items-center justify-between space-y-0'>
+      <Skeleton className='h-8 w-36' />
+      <Skeleton className='h-10 w-[4.625rem] sm:w-[9.5rem]' />
+    </CardHeader>
+    <CardContent className='grid h-[22rem] place-items-center'>
+      <Spinner />
+    </CardContent>
+  </Card>
+);
+
 export const TransactionsChartCard = () => {
-  const { summary } = useSummary();
+  const { summary, summaryLoading } = useSummary();
   const [chartType, setChartType] = useState<TransactionsChartType>('area');
 
+  if (summaryLoading) return <TransactionsChartCardLoading />;
   return (
     <Card className='col-span-1 lg:col-span-3 xl:col-span-4'>
       <CardHeader className='flex flex-row items-center justify-between space-y-0'>
