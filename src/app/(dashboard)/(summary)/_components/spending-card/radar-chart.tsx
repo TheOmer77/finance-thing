c936f6af5c7f1dceb378965e4bc7e86372e57212
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-23 14:57:39 +0300

    Add radar chart to dashboard spending card

--- src/app/(dashboard)/(summary)/_components/spending-card/radar-chart.tsx
+++ src/app/(dashboard)/(summary)/_components/spending-card/radar-chart.tsx
@@ -1,5 +1,27 @@
+import {
+  RadarChart as Chart,
+  PolarAngleAxis,
+  PolarGrid,
+  PolarRadiusAxis,
+  Radar,
+  ResponsiveContainer,
+} from 'recharts';
+
 import type { SpendingChartProps } from './types';
 
-export const RadarChart = ({ data }: SpendingChartProps) => {
-  return <div>Radar chart TBD</div>;
-};
+export const RadarChart = ({ data }: SpendingChartProps) => (
+  <ResponsiveContainer className='!h-[22rem]'>
+    <Chart cx='50%' cy='50%' outerRadius='60%' data={data}>
+      <PolarGrid />
+      <PolarAngleAxis className='text-xs' dataKey='name' />
+      <PolarRadiusAxis className='text-xs' />
+
+      <Radar
+        dataKey='value'
+        stroke='hsl(var(--primary))'
+        fill='hsl(var(--primary))'
+        fillOpacity={0.6}
+      />
+    </Chart>
+  </ResponsiveContainer>
+);
