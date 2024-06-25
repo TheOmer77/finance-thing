8e4196cd0b22997ff93574851d69517ef0b2caec
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-25 17:18:08 +0300

    Update Calendar nav buttons style
    Remove weird nav button style that makes them look like they're disabled
    unless hovered.

--- src/components/ui/Calendar/Calendar.tsx
+++ src/components/ui/Calendar/Calendar.tsx
@@ -24,10 +24,7 @@ export const Calendar = ({
       caption: 'flex justify-center pt-1 relative items-center',
       caption_label: 'text-sm font-medium',
       nav: 'space-x-1 flex items-center',
-      nav_button: cn(
-        buttonVariants({ variant: 'outline' }),
-        'size-7 bg-transparent p-0 opacity-50 hover:opacity-100'
-      ),
+      nav_button: cn(buttonVariants({ variant: 'outline' }), 'size-7 p-0'),
       nav_button_previous: 'absolute left-1',
       nav_button_next: 'absolute right-1',
       table: 'w-full border-collapse space-y-1',
