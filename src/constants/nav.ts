125060464b5f71245bed73f019b36b386678c4ed
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-25 17:25:44 +0300

    Remove settings from nav

--- src/constants/nav.ts
+++ src/constants/nav.ts
@@ -3,5 +3,4 @@ export const navRoutes = [
   { href: '/transactions', label: 'Transactions' },
   { href: '/accounts', label: 'Accounts' },
   { href: '/categories', label: 'Categories' },
-  { href: '/settings', label: 'Settings' },
 ] as const satisfies { href: string; label: string }[];
