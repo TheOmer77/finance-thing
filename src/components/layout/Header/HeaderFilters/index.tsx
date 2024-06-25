2cd821763b8ff55eb7549c92ea174da7d7dcebfc
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-25 15:29:13 +0300

    Hide header filters where they're not used
    Only show header filters in overview and transactions pages, otherwise
    hide them as they're not needed.
    Also adjust the negative margin of the page's main content based on
    whether or not the headers are visible.

--- src/components/layout/Header/HeaderFilters/index.tsx
+++ src/components/layout/Header/HeaderFilters/index.tsx
@@ -1,9 +1,17 @@
+'use client';
+
+import { usePathname } from 'next/navigation';
+
 import { AccountFilter } from './AccountFilter';
 
+const VISIBLE_PATHNAMES = ['/', '/transactions'];
+
 export const HeaderFilters = () => {
-  // TODO: Only show in summary & transactions pages
+  const pathname = usePathname();
+  if (!VISIBLE_PATHNAMES.includes(pathname)) return null;
+
   return (
-    <div className='flex flex-row flex-wrap items-center gap-2'>
+    <div className='flex flex-row flex-wrap items-center gap-2 [header:has(&)+*]:-mt-10 lg:[header:has(&)+*]:-mt-16'>
       <AccountFilter />
     </div>
   );
--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -1,12 +1,12 @@
-import { HeaderLogo } from './HeaderLogo';
 import { Nav } from '@/components/layout/Nav';
 import { UserMenu } from '@/components/layout/UserMenu';
 
+import { HeaderLogo } from './HeaderLogo';
 import { HeaderWelcomeMsg } from './HeaderWelcomeMsg';
 import { HeaderFilters } from './HeaderFilters';
 
 export const Header = () => (
-  <header className='h-64 w-full bg-background bg-gradient-to-b from-primary to-primary/80 text-primary-foreground lg:h-80 [&+*]:-mt-10 lg:[&+*]:-mt-16 [&>*]:mx-auto [&>*]:w-full [&>*]:max-w-screen-2xl [&>*]:px-4'>
+  <header className='h-64 w-full bg-background bg-gradient-to-b from-primary to-primary/80 text-primary-foreground lg:h-80 [&+*]:-mt-24 lg:[&+*]:-mt-28 [&>*]:mx-auto [&>*]:w-full [&>*]:max-w-screen-2xl [&>*]:px-4'>
     <div className='flex h-16 items-center justify-start gap-2 lg:flex-row lg:gap-8'>
       <HeaderLogo />
       <Nav />
