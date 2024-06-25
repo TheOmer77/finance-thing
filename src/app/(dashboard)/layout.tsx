3c89514bf9130f799eaf2fda9cdef9e83ef04f11
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-25 17:38:41 +0300

    Add bottom margin to dashboard main content

--- src/app/(dashboard)/layout.tsx
+++ src/app/(dashboard)/layout.tsx
@@ -5,7 +5,9 @@ import { Header } from '@/components/layout/Header';
 const DashboardLayout = ({ children }: PropsWithChildren) => (
   <>
     <Header />
-    <main className='mx-auto w-full max-w-screen-2xl px-4'>{children}</main>
+    <main className='mx-auto mb-4 w-full max-w-screen-2xl px-4'>
+      {children}
+    </main>
   </>
 );
 
