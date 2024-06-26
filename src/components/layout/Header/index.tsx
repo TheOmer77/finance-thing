27abed06e04a620cc7ce3464c77e8b4403c49ec9
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-26 15:48:44 +0300

    Update header gradient in dark mode

--- src/components/layout/Header/index.tsx
+++ src/components/layout/Header/index.tsx
@@ -7,7 +7,7 @@ import { HeaderFilters } from './HeaderFilters';
 import { HeaderThemeMenu } from './HeaderThemeMenu';
 
 export const Header = () => (
-  <header className='h-64 w-full bg-background bg-gradient-to-b from-primary to-primary/80 text-white lg:h-80 [&+*]:-mt-24 lg:[&+*]:-mt-28 [&>*]:mx-auto [&>*]:w-full [&>*]:max-w-screen-2xl [&>*]:px-4'>
+  <header className='h-64 w-full bg-background bg-gradient-to-b from-primary to-primary/80 text-white lg:h-80 dark:from-primary/10 dark:to-primary/20 [&+*]:-mt-24 lg:[&+*]:-mt-28 [&>*]:mx-auto [&>*]:w-full [&>*]:max-w-screen-2xl [&>*]:px-4'>
     <div className='flex h-16 items-center justify-start gap-2 lg:flex-row lg:gap-8'>
       <HeaderLogo />
       <Nav />
