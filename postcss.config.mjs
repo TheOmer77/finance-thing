116bae48048ba2f2cc6bb7b5cc98c9c90961d167
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-26 15:21:47 +0300

    Add `postcss-mixins`

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('postcss-load-config').Config} */
 const config = {
   plugins: {
+    'postcss-mixins': {},
     tailwindcss: {},
     'postcss-hover-media-feature': {},
     'postcss-viewport-unit-fallback': {},
