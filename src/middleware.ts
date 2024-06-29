a133857ccb6d84c32ad1ee937aceb5d3587d9c5f
Omer <27678413+TheOmer77@users.noreply.github.com>
2024-06-29 13:33:30 +0300

    Fix no redirect from auth page if signed in

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,12 +1,20 @@
+import { NextResponse } from 'next/server';
 import { clerkMiddleware, createRouteMatcher } from '@clerk/nextjs/server';
 
 import { navRoutes } from '@/constants/nav';
 
 const protectedRoutes = navRoutes.map(({ href }) => href),
-  isProtectedRoute = createRouteMatcher(protectedRoutes);
+  isProtectedRoute = createRouteMatcher(protectedRoutes),
+  isAuthRoute = createRouteMatcher(['/signin', '/signup']);
 
 export default clerkMiddleware((auth, req) => {
-  if (isProtectedRoute(req)) auth().protect();
+  const { userId, protect } = auth();
+  if (isProtectedRoute(req)) protect();
+  if (isAuthRoute(req) && userId)
+    return NextResponse.redirect(
+      req.nextUrl.searchParams.get('redirect_url') ||
+        process.env.NEXT_PUBLIC_APP_URL!
+    );
 });
 
 export const config = {
